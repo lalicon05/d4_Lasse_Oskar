444baa8227c02a4e48e9bf5a263d644f24cf7d5c
lalicon05 <lasse.l535@gmail.com>
2025-11-25 20:00:16 +0100

    added comment

--- examples/run_sim.py
+++ examples/run_sim.py
@@ -41,6 +41,7 @@ def print_results(results: list):
     avg_kaia_step = round(kaia_step / len(kaia_results), 2)
     avg_pent_step = round(pent_step / len(pent_results), 2)
 
+    # prints out the data
     print(f"Kaia ends: {len(kaia_results)}, Pentagon ends: {len(pent_results)}")
     print(f"avg kaia time: {avg_kaia_sec}, avg pent time: {avg_pent_sec}")
     print(f"avg kaia step: {avg_kaia_step}, avg pent step: {avg_pent_step}")
